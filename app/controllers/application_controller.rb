8dafc8b7f524ddf5fff6c0fa058fbe0367e03bdb
zuca <2682zucazuca@gmail.com>
2021-01-22 14:47:33 +0900

    ユーザー管理機能を実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,14 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  private
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:kana_last_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:kana_first_name])
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+  end
 end
