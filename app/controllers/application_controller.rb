b6e9b5252b4da00d92d1a4446b52a0031f1f857a
zuca <2682zucazuca@gmail.com>
2021-01-22 17:40:23 +0900

    devise_parameter簡略化

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,11 +4,7 @@ class ApplicationController < ActionController::Base
   private
 
   def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:kana_last_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:kana_first_name])
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:birthday])
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :kana_last_name, :kana_first_name, :birthday])
   end
 end
