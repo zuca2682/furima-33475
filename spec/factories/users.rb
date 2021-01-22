b3692213216ba81fc8d49e98f66e911af8125cbc
zuca <2682zucazuca@gmail.com>
2021-01-22 19:07:59 +0900

    パスワードのFaker修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname { Faker::Name.initials }
     email { Faker::Internet.free_email }
-    password = Faker::Internet.password(min_length: 6)
+    password = '1a' + Faker::Internet.password(min_length: 4)
     password { password }
     password_confirmation { password }
     last_name { Faker::Japanese::Name.last_name }
