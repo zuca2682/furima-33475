32fc17e0c367223af4f4a9362fc4084b83a63d38
zuca <2682zucazuca@gmail.com>
2021-01-22 19:06:35 +0900

    名前、カナ名をネスト構造へ修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,10 +8,17 @@ class User < ApplicationRecord
 
   with_options presence: true do
     validates :nickname
-    validates :last_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :first_name, format: { with: /\A[ぁ-んァ-ン一-龥]/ }
-    validates :kana_last_name, format: { with: /\A[ァ-ヶー－]+\z/ }
-    validates :kana_first_name, format: { with: /\A[ァ-ヶー－]+\z/ }
+    
+    with_options  format: { with: /\A[ぁ-んァ-ン一-龥]/ } do
+      validates :last_name
+      validates :first_name
+    end
+
+    with_options format: { with: /\A[ァ-ヶー－]+\z/ } do
+      validates :kana_last_name
+      validates :kana_first_name
+    end
+
     validates :birthday
   end
 end
