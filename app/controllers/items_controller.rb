7233f66691c6b2beae808cad4fcba4e61e305f94
zuca <2682zucazuca@gmail.com>
2021-01-24 16:41:17 +0900

    出品ページへのログイン・アウト遷移先設定

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,26 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new]
+
   def index
+    @items = Item.all
+  end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:name, :explain, :category_id, :state_id, :burden_id, :area_id, :delivary_id, :price )
   end
 end
