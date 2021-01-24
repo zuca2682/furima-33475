11751aa1da47f4f40c96bb134c520142ec48a409
zuca <2682zucazuca@gmail.com>
2021-01-24 16:43:06 +0900

    ActiveHash導入,validatesの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,26 @@ class Item < ApplicationRecord
   has_one_attached :order
   has_one_attached :image
 
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :state
+  belongs_to :burden
+  belongs_to :area
+  belongs_to :delivery
+
+  with_options presence: true do
+    validates :name
+    validates :explain
+    validates :image
+    
+    with_options  numericality: { other_than: 1 } do
+      validates :category_id
+      validates :state_id
+      validates :burden_id
+      validates :area_id
+      validates :delivery_id
+    end
+
+    validates :price, foramt: {with: /^[0-9]+$/}, numericality: { only_integer:true, greater_than:300, less_than: 9999999}
+  end
 end
