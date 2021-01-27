83086b47fd4706473664a0e48ab15d78b8c5d9b0
zuca <2682zucazuca@gmail.com>
2021-01-27 16:13:47 +0900

    destroyアクションに条件分岐追加

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -36,7 +36,11 @@ class ItemsController < ApplicationController
 
   def destroy
     @item.destroy
-    redirect_to root_path
+    if @item.user == current_user
+      redirect_to root_path
+    else
+      render :show
+    end
   end
 
   private
